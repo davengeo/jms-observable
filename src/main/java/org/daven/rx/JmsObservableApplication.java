bc4216d262b9e964d00ba8f58a1d8869e0184df6
David Navarro <id843270@proximus.com>
2015-10-09 12:38:48 +0200

    Initial commit

---
+++ src/main/java/org/daven/rx/JmsObservableApplication.java
@@ -0,0 +1,12 @@
+package org.daven.rx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsObservableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmsObservableApplication.class, args);
+    }
+}
