2d670ab8d660f1f7e57483cadcb15746894825ae
David Navarro <id843270@proximus.com>
2015-10-12 16:24:07 +0200

    absolutely async

--- src/main/java/org/daven/rx/controllers/DeferredController.java
+++ src/main/java/org/daven/rx/controllers/DeferredController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
@@ -23,18 +24,18 @@ public class DeferredController {
     @Autowired
     JmsListener listener;
 
-    @RequestMapping("/send-receive")
-    public DeferredResult<HttpEntity<String>> sendAndreceive() {
+    @RequestMapping("/send-receive/{message}")
+    public DeferredResult<HttpEntity<String>> sendAndReceive(@PathVariable String message) {
 
         DeferredResult<HttpEntity<String>> deferred = new DeferredResult<>(90000);
         final Observable<EventContainer> filtered = listener.jmsStream().
-                takeFirst(eventContainer1 -> eventContainer1.getBody().contains("jajaja!"));
+                takeFirst(eventContainer1 -> eventContainer1.getBody().contains(message));
         filtered.subscribe(
-                eventContainer -> deferred.setResult(new HttpEntity<>("SENT " + eventContainer.getBody())),
+                eventContainer -> deferred.setResult(new HttpEntity<>("RECEIVED " + eventContainer.getBody())),
                 throwable -> deferred.setErrorResult(new HttpEntity<>("ERROR")),
                 () -> LOG.debug("completed"));
         jmsTemplate.send("mailbox-destination",
-                session -> session.createTextMessage("jajaja!"));
+                session -> session.createTextMessage(message));
         return deferred;
     }
 }
