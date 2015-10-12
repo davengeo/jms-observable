1df8a07528a277be4ac879a3ef04b6f5005b6341
David Navarro <id843270@proximus.com>
2015-10-12 11:01:18 +0200

    with echo rest endpoint

--- src/main/java/org/daven/rx/controllers/DeferredController.java
+++ src/main/java/org/daven/rx/controllers/DeferredController.java
@@ -11,34 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
 import rx.Observable;
-import rx.schedulers.Schedulers;
 
 @RestController
 public class DeferredController {
 
     private final static Logger LOG = LoggerFactory.getLogger(DeferredController.class);
-
-    @RequestMapping("/get-async")
-    public DeferredResult<String> getAsync() {
-        Observable<String> o = getAMessageObs();
-        DeferredResult<String> deferred = new DeferredResult<>(90000);
-        o.subscribe(deferred::setResult, deferred::setErrorResult);
-        return deferred;
-    }
-
-    public Observable<String> getAMessageObs() {
-        return Observable.<String>create(s -> {
-            LOG.info("Start: Executing slow task in Service 1");
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            s.onNext("data 1");
-            LOG.info("End: Executing slow task in Service 1");
-            s.onCompleted();
-        }).subscribeOn(Schedulers.io());
-    }
 
     @Autowired
     JmsTemplate jmsTemplate;
